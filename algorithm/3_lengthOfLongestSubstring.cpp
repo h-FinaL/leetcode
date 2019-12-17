4cb3a4279c6b4f511e7404c9ab08f0a80d56d999
unknown <huzhihao@rt.com>
2019-12-17 09:51:19 +0800

    无重复字符的最长字串

---
+++ algorithm/3_lengthOfLongestSubstring.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+ 
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		const char* c = s.c_str();
+		std::map<char, int> m;
+		int maxlen = 0;
+		int first = 0;
+		int last = 0;
+		for (int i = 0; i <= s.length(); i++)
+		{
+			if (m.find(c[i]) == m.end())
+			{
+				m[c[i]] = i;
+			}
+			else
+			{
+				if (i - first > maxlen)
+					maxlen = i - first;
+				last = m[c[i]] + 1;
+				for (int j = first; j <= m[c[i]]; j++)
+				{
+					m.erase(c[j]);
+				}
+				m[c[i]] = i;
+				first = last;
+			}
+		}
+
+		if (s.length() - first > maxlen)
+			maxlen = s.length() - first;
+
+		return maxlen;
+	}
+};
