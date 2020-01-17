f14b8594e6ef313dc634f109d07877ff5a08e671
unknown <huzhihao@rt.com>
2020-01-17 10:56:07 +0800

    盛最多水对容器

---
+++ algorithm/11_maxArea.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+		int max = 0;
+		int left = 0;
+		int right = height.size() - 1;
+		int m_height;
+		int length;
+		while (left < right)
+		{
+			m_height = height[left] < height[right] ? height[left] : height[right];
+			length = right - left;
+			if (height[left] < height[right])
+			{
+				m_height = height[left];
+				left++;
+			}
+			else
+			{
+				m_height = height[right];
+				right--;
+			}
+			if (max < m_height * length)
+				max = m_height * length;
+		}
+		return max;
+	}
+};
