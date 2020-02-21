a1a5d74929ae467183ab1d1dd082f2f8013eef6b
unknown <619479768@qq.com>
2020-02-21 18:19:08 +0800

    排序数组中查找元素第一个和最后一个位置

---
+++ algorithm/34_searchRange.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> searchRange(vector<int>& nums, int target) {
+		vector<int> result{ -1, -1 };
+		if (nums.size() == 0)
+			return result;
+		int left = 0;
+		int right = nums.size() - 1;
+		int mid;
+
+		while (left < right)
+		{
+			mid = (left + right) / 2;
+			if (nums[mid] >= target)
+				right = mid;
+			else
+				left = mid + 1;
+		}
+
+		if (nums[right] == target)
+			result[0] = right;
+		else
+			return result;
+
+		left = right;
+		right = nums.size() - 1;
+		while (left < right)
+		{
+			mid = (left + right) / 2;
+			if (nums[mid] > target)
+				right = mid;
+			else
+				left = mid + 1;
+		}
+		if (nums[left] == target)
+			result[1] = left;
+		else
+			result[1] = left - 1;
+
+		return result;
+	}
+};
