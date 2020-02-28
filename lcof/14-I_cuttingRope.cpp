d8371e88c0b0b0b41a61272217c0a0e72ed2cd8a
unknown <619479768@qq.com>
2020-02-28 15:07:34 +0800

    剪绳子

---
+++ lcof/14-I_cuttingRope.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+class Solution {
+public:
+	int cuttingRope(int n) {
+		if (n == 2) return 1;
+		if (n == 3) return 2;
+		int result = 1;
+		int num = n / 3;
+		if (n % 3 == 1)
+		{
+			result = 4;
+			num--;
+		}
+		else if (n % 3 == 2)
+			result = 2;
+		for (int i = 0; i < num; i++)
+		{
+			result = result * 3;
+		}
+
+		return result;
+	}
+};
