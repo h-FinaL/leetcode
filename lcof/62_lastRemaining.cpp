a0f150d8c5f75e3e0f700efbc0768d4773521ad1
unknown <619479768@qq.com>
2020-03-25 14:36:41 +0800

    圆圈中最后剩下的数字

---
+++ lcof/62_lastRemaining.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+ 
+class Solution {
+public:
+	int lastRemaining(int n, int m) {
+		int k = 0;
+		for (int i = 2; i <= n; i++) {
+			k = (k + m) % i;
+		}
+		return k;
+	}
+};
