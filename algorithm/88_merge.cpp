6e06dca35ca08cccbd1e58ecf30461751e0d789e
unknown <619479768@qq.com>
2020-03-30 14:55:19 +0800

    合并两个有序数组

---
+++ algorithm/88_merge.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		if (m + n == 0) return;
+		int i1 = m - 1;
+		int i2 = n - 1;
+		int i = m + n - 1;
+		while (i1 >= 0 && i2 >= 0)
+		{
+			if (nums1[i1] > nums2[i2])
+			{
+				nums1[i] = nums1[i1];
+				i1--;
+			}
+			else
+			{
+				nums1[i] = nums2[i2];
+				i2--;
+			}
+			i--;
+		}
+		while (i2 >= 0)
+		{
+			nums1[i] = nums2[i2];
+			i2--;
+			i--;
+		}
+	}
+};
