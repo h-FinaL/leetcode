55ecea9a3cdf59a7678b97117f9c81d6959fc4f5
unknown <619479768@qq.com>
2020-03-31 13:29:52 +0800

    杨辉三角II

---
+++ algorithm/119_getRow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+class Solution {
+public:
+	vector<int> getRow(int rowIndex) {
+		vector<int> v(rowIndex, 1);
+		for (int i = 1; i <= rowIndex; i++)
+		{
+			for (int j = i - 2; j >= 1; j--)
+				v[j] = v[j] + v[j - 1];
+		}
+		return v;
+	}
+};
