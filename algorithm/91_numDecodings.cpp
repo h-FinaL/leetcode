27bd3a00644f1dd5b617938c52d2cee4905b6d13
unknown <619479768@qq.com>
2020-05-28 16:31:02 +0800

    解码方法 第二次完成

--- algorithm/91_numDecodings.cpp
+++ algorithm/91_numDecodings.cpp
@@ -8,7 +8,28 @@
 using namespace std;
 class Solution {
 public:
+	//第二次完成
 	int numDecodings(string s) {
+		if (s.empty() || s[0] == '0')
+			return 0;
+		int dp1 = 1;
+		int dp2 = 1;
+		for (int i = 1; i < s.size(); i++)
+		{
+			int temp = dp2;
+			dp2 = 0;
+			int n = (s[i - 1] - '0') *10 + s[i] - '0';
+			if (10 <= n && n <= 26)
+				dp2 += dp1;
+			if (s[i] > '0')
+				dp2 += temp;
+			dp1 = temp;
+		}
+
+		return dp2;
+	}
+
+	int numDecodings1(string s) {
 		if (s.empty())
 			return 0;
 		vector<int> dp(s.size() + 1, 0);
