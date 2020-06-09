2eda7960b9da18b4bda88b2190bd902e406e89d7
unknown <619479768@qq.com>
2020-06-09 10:37:28 +0800

    把数字翻译成字符串

--- lcof/46_translateNum.cpp
+++ lcof/46_translateNum.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <vector>
 
+/*
+面试题46. 把数字翻译成字符串
+*/
+
 using namespace std;
 
 class Solution {
 public:
+    //第二次实现 动态规划
+	int translateNum1(int num) {
+		if (num == 0)
+			return 1;
+		stack<int> stk;
+		while (num)
+		{
+			stk.push(num % 10);
+			num = num / 10;
+		}
+		int dp1 = 1, dp2 = 0;
+		int pre;
+		if (!stk.empty())
+		{
+			pre = stk.top();
+			dp2 = 1;
+			stk.pop();
+		}
+		while (!stk.empty())
+		{
+			int tmp = dp2;
+			int n = stk.top() + pre * 10;
+			if (10 <= n && n <= 25)
+			{
+				dp2 += dp1;
+			}
+			pre = stk.top();
+			stk.pop();
+			dp1 = tmp;
+		}
+		return dp2;
+	}
+
     int translateNum(int num) {
         vector<int> n;
         while (num > 0)
