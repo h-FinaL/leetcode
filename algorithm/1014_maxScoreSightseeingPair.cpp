af4b15aa159e40e09ce480220984eb96f7143a4c
unknown <619479768@qq.com>
2020-06-17 10:46:32 +0800

    最佳观光组合

---
+++ algorithm/1014_maxScoreSightseeingPair.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*
+1014. 最佳观光组合
+给定正整数数组 A，A[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的距离为 j - i。
+一对景点（i < j）组成的观光组合的得分为（A[i] + A[j] + i - j）：景点的评分之和减去它们两者之间的距离。
+返回一对观光景点能取得的最高分。
+*/
+
+using namespace std;
+class Solution {
+public:
+	int maxScoreSightseeingPair(vector<int>& A) {
+		int res = 0;
+		int dp = A[0];
+		for (int i = 1; i < A.size(); i++)
+		{
+			res = max(res, A[i] + dp - 1);
+			dp = max(dp - 1, A[i]);
+		}
+		return res;
+	}
+};
