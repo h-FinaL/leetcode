eea6b9f2be1465d8aa9ab2e7f7cd61e3ef430b02
unknown <619479768@qq.com>
2020-12-18 16:24:21 +0800

    二叉搜索树中的插入操作

---
+++ algorithm/701_insertIntoBST.cpp
@@ -0,0 +1,34 @@
+/*
+701. 二叉搜索树中的插入操作
+
+给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+
+注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode** node = &root;
+        while (*node)
+        {
+            if ((*node)->val > val)
+                node = &(*node)->left;
+            else
+                node = &(*node)->right;
+        }
+        *node = new TreeNode(val);
+        return root;
+    }
+};
